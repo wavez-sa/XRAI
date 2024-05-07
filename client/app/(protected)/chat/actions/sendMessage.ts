f37f8d65581254f6fffa6ce9397af2a0e5ec899f
MajidRaimi <imajid2000z1@gmail.com>
2024-05-07 20:15:34 +0300

    MR

--- client/app/(protected)/chat/actions/sendMessage.ts
+++ client/app/(protected)/chat/actions/sendMessage.ts
@@ -15,11 +15,7 @@ const sendMessage = async (formData: SendMessageProps) => {
     const { message, image } = formData;
     if (!message && !image) return;
 
-<<<<<<< HEAD
     await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/chat', {
-=======
-    await fetch('https://api.xrai.dev/chat', {
->>>>>>> 9431daf407adf8c4d5ee73671722506d19ced579
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
